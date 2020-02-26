3b1ea8980bf20f90005398f90fa6dc19b0bcfa9b
Jonathan Smalls <jon@smalls.rocks>
2020-02-25 21:54:33 -0500

    refactor: using CSV module according to instructions

--- tsvPractice/index.py
+++ tsvPractice/index.py
@@ -13,26 +13,3 @@ eating disorder                1.2406    1.1493    1.8994    1.3713    0.8086
 
 You must use module csv for reading and writing the files.
 """
-def main():
-    inputFile  = open('freqs.tsv', 'r')
-    outputFile = open('freqs-mean.tsv', 'w')
-    for inputLine in inputFile:
-        lineList = inputLine.split('\t')
-        lineAverage = 0;
-        # The last entry in a line is the new line, so we need to exclude that
-        # from our calculations.
-        for loop in range(1, len(lineList) - 1):
-            if '' == lineList[loop]:
-                lineList[loop] = 0
-            lineAverage += float(lineList[loop])
-        # We need to account for the first column, which is a word, not a
-        # number. We also need to account for the last column, which will be the
-        # uncountable new line character.
-        lineAverage = str(lineAverage / (len(lineList) - 2))
-        print(
-            "The average frequency for " + lineList[0] + " is " + lineAverage
-        )
-        outputLine = inputLine + '\t' + lineAverage + '\t\n'
-        outputFile.write(outputLine)
-
-main()
